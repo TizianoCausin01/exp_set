931b8b0dda816946bb24e1724c1619a8224df094
TizianoCausin01 <causintiziano@gmail.com>
2025-06-17 11:54:52 +0200

    made response_freq more efficient

--- python_scripts/src/sparsity_in_silico/sparsity_CNN.py
+++ python_scripts/src/sparsity_in_silico/sparsity_CNN.py
@@ -4,16 +4,12 @@ import torch
 def response_prob(feats_dict):
     p_feat_dict = {}
     for k, v in feats_dict.items():
-        print(k)
-        print(v[0].size())
         n_neu = feats_dict[k][0].size()
         n_stim = len(feats_dict[k])
         freq_fire = torch.zeros(n_neu, dtype=torch.uint8)
-        for risp in v:
-            bool_risp = (risp > 0).to(dtype=torch.uint8)
-            freq_fire += bool_risp.to(dtype=torch.uint8)
-        # end for risp in v:
-        p_feat_dict[k] = freq_fire / n_stim
+        stack_resp = torch.stack(v, dim=1) 
+        freq_fire = (stack_resp > 0).sum(dim=1)
+        p_feat_dict[k] = freq_fire.float() / n_stim
     return p_feat_dict
 
     # end for k, v in feats_dict.items():
