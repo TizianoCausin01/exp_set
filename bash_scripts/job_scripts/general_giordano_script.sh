4a1c4b1ed0d0370b3404df91358c2c5c62fe808f
Tiziano Causin <tcausin0@login07.leonardo.local>
2025-07-08 00:45:46 +0200

    Created a bash script to launch the models job together

---
+++ bash_scripts/job_scripts/general_giordano_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1 # number of processes
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --mem=480G
+#SBATCH --account=Sis25_piasini       # account name
+#SBATCH --partition=boost_usr_prod # partition name
+# #SBATCH --job-name=giordano_alexnet_PCA
+#SBATCH --output=/leonardo/home/userexternal/tcausin0/output_jobs/%x.%j.out   # file name will be *job_name*.*job_id*
+cd /leonardo/home/userexternal/tcausin0/exp_set/python_scripts/scripts/leonardo_scripts
+module load python/3.11.6--gcc--8.5.0
+source ~/virtual_envs/ponce_env/bin/activate
+python giordano_feat_extraction_PCA.py \
+  --model_name $1 \
+  --n_components $2 \
+  --batch_size 1024 \
+  --num_workers $SLURM_NTASKS \
+  --multiple_passes
---
+++ bash_scripts/job_scripts/launch_PCA_extraction.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# $1 -> the models to test, in this format "model1 model2 ..."
+# $2 -> the number of PCs to compute
+PCs=$2
+read -a models <<< "$1"    # reads the first argin and creates an array called files
+for m in "${models[@]}"; do
+    sbatch --job-name=${m}_PCA_${PCs}_PCs ./general_giordano_script.sh $m $PCs
+done
