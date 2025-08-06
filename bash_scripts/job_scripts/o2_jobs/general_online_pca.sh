a9df59bcc6cf1fe8e62ea16342571c331e2a5a52
TizianoCausin01 <causintiziano@gmail.com>
2025-08-06 18:07:48 -0400

    sh script for PCA

---
+++ bash_scripts/job_scripts/o2_jobs/general_online_pca.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --nodes=1 
+#SBATCH --time=24:00:00 
+#SBATCH --ntasks=4 # number of processes 
+#SBATCH -p gpu_quad
+#SBATCH --gres=gpu:rtx8000:1
+#SBATCH --cpus-per-task=1 
+#SBATCH --mem=250G 
+# #SBATCH --account=       # account name 
+# #SBATCH --partition=priority # partition name 
+#SBATCH --job-name=online_pca 
+#SBATCH --output=/home/tic569/output_jobs/%x.%j.out   # file name will be *job_name*.*job_id* 
+cd /home/tic569/exp_set/python_scripts/scripts 
+module load conda/miniforge3/24.11.3-0 
+module load gcc/14.2.0 
+module load python/3.13.1 
+conda activate ponce_env 
+python3 run_online_PCA.py --model_name $1 --n_components $2 --batch_size $3 --num_workers $SLURM_NTASKS
