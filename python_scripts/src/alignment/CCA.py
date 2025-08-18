9733ab5273f4e6a8b22663352d0fc603a10c294e
TizianoCausin01 <causintiziano@gmail.com>
2025-08-18 17:56:58 -0400

    Added CKA and functions

--- python_scripts/src/alignment/CCA.py
+++ python_scripts/src/alignment/CCA.py
@@ -13,98 +13,6 @@ sys.path.append("..")
 from dim_redu_anns.utils import get_relevant_output_layers
 
 
-def quickCCA_loop_within_mod(model_name, pooling, num_components, res_path):
-    """
-    Name:
-        quickCCA_loop_within_mod
-
-    Description:
-        Performs Canonical Correlation Analysis (CCA) between all pairs of layers from a given model.
-        For each pair of layers, it loads precomputed activations (features), computes the CCA,
-        and saves the resulting projection weights and canonical correlation coefficients.
-        It also stores and saves a symmetric similarity matrix representing the mean CCA correlation
-        between all layer pairs.
-
-    Inputs:
-        model_name (str):
-            The name of the model architecture (e.g., 'resnet18', 'alexnet').
-
-        pooling (str):
-            Pooling method used in feature extraction ('all', 'avgpool', 'maxpool').
-
-        num_components (int):
-            Number of CCA components to compute between each pair of layers.
-
-        res_path (str):
-            Root directory where extracted features and CCA results will be read from and written to.
-
-    Outputs:
-        None. Saves:
-            - CCA weights and canonical correlations for each pair of layers as `.pkl` files.
-            - A CSV file (`{model_name}_similarity_layers.csv`) with the mean CCA similarity
-              for each layer pair.
-
-    Example usage:
-        CCA_loop_within_mod("resnet18", "avgpool", 20, "./results")
-
-    Notes:
-        - This performs only the lower triangle of the layer-to-layer comparison matrix (layer2 < layer1).
-        - Assumes feature files have already been extracted and saved at:
-              {res_path}/imagenet_val_{model_name}_{layer}_{pooling}_features.pkl
-    """
-
-    layer_names = get_relevant_output_layers(model_name)
-    cca_dir = f"{res_path}/cca_{model_name}_{pooling}"
-    os.makedirs(cca_dir, exist_ok=True)
-    layers_RSA = np.zeros((len(layer_names), len(layer_names)))
-    for layer_idx1 in range(len(layer_names)):
-        target_layer1 = layer_names[layer_idx1]
-        feats_path1 = f"{res_path}/imagenet_val_{model_name}_{target_layer1}_{pooling}_features.pkl"
-        all_acts1 = joblib.load(feats_path1)
-        for layer_idx2 in range(layer_idx1):
-            target_layer2 = layer_names[layer_idx2]
-            print(
-                datetime.now().strftime("%H:%M:%S"),
-                f"stating layers {target_layer1} vs {target_layer2}",
-            )
-            feats_path2 = f"{res_path}/imagenet_val_{model_name}_{target_layer2}_{pooling}_features.pkl"
-            save_path = f"{cca_dir}/cca_{model_name}_{num_components}_components_{target_layer1}_vs_{target_layer2}.pkl"
-            if os.path.exists(save_path):
-                print(
-                    datetime.now().strftime("%H:%M:%S"),
-                    f"CCA already exists for {target_layer1} vs {target_layer2}  at {save_path}",
-                    flush=True,
-                )
-            else:
-                all_acts2 = joblib.load(feats_path2)
-                cca = CCA(n_components=num_components, max_iter=1000)
-                cca.fit(all_acts1, all_acts2)
-                weights_dict = {}
-                weights_dict["W1"] = (
-                    cca.x_weights_
-                )  # shape: (n_features1, n_components)
-                weights_dict["W2"] = (
-                    cca.y_weights_
-                )  # shape: (n_features2, n_components)
-
-                # 3. Project the data manually (optional, equivalent to fit_transform)
-                d1 = all_acts1 @ weights_dict["W1"]
-                d2 = all_acts2 @ weights_dict["W2"]
-                coefs_CCA = np.array(
-                    [np.corrcoef(d1[:, i], d2[:, i])[0, 1] for i in range(d1.shape[1])]
-                )
-                weights_dict["coefs"] = coefs_CCA
-                joblib.dump(weights_dict, save_path)
-                layers_RSA[layer_idx1, layer_idx2] = np.mean(coefs_CCA)
-                print(
-                    datetime.now().strftime("%H:%M:%S"),
-                    f"{target_layer1} vs {target_layer2} corr {np.round(np.mean(coefs_CCA), 3)}",
-                    flush=True,
-                )
-    csv_save_path = f"{cca_dir}/{model_name}_similarity_layers.csv"
-    np.savetxt(csv_save_path, layers_RSA, delimiter=",")
-
-
 def CCA_loop_within_mod(model_name, pooling, num_components, pca_option, res_path):
     layer_names = get_relevant_output_layers(model_name)
     cca_dir = f"{res_path}/cca_{model_name}_{pooling}"
@@ -355,3 +263,7 @@ def CCA_loop_between_mod(model_names, pooling, num_components, pca_option, res_p
             f"{cca_dir}/{model_names[0]}_vs_{model_names[1]}_similarity_layers_pca.csv"
         )
     np.savetxt(csv_save_path, layers_RSA, delimiter=",")
+
+
+
+
--- python_scripts/src/alignment/__init__.py
+++ python_scripts/src/alignment/__init__.py
@@ -1,6 +1,7 @@
 __all__ = [
-  'get_maxpool_evecs', 'sample_features', 'get_usual_transform', 'features_extraction_loop', 'quickCCA_loop_within_mod', 'CCA_loop_within_mod', 'CCA_loop_between_mod']
+  'get_maxpool_evecs', 'sample_features', 'get_usual_transform', 'features_extraction_loop', 'CCA_loop_within_mod', 'CCA_loop_between_mod', 'cka', 'cka_minibatch', 'cka_batch_collection']
 
 from .utils import get_maxpool_evecs, sample_features, get_usual_transform, features_extraction_loop
-from .CCA import quickCCA_loop_within_mod, CCA_loop_within_mod, CCA_loop_between_mod
+from .CCA import CCA_loop_within_mod, CCA_loop_between_mod
+from .CKA import cka, cka_minibatch, cka_batch_collection
 
